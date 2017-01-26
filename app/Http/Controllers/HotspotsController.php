f2abd630fc49e68618a563f409d28f1e4c706ba6
Soy Caringal <soycaringal@github.com>
2017-01-27 01:24:37 +0800

    Initial Commit

---
+++ app/Http/Controllers/HotspotsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Hotspot;
+use Illuminate\Http\Request;
+
+class HotspotsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Hotspot  $hotspot
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Hotspot $hotspot)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Hotspot  $hotspot
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Hotspot $hotspot)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Hotspot  $hotspot
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Hotspot $hotspot)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Hotspot  $hotspot
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Hotspot $hotspot)
+    {
+        //
+    }
+}
---
+++ resources/views/admin/users/index.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: soy
+ * Date: 1/27/17
+ * Time: 12:31 AM
+ */
